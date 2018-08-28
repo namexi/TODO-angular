a77cb785584c03cd303dab4c2818e869470997fe
namexi <406633257@qq.com>
2018-08-28 15:17:00 +0800

    04-初始化成员行为

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@
 		// 初始化 页面成员
 		$scope.input = ''
 		$scope.todoList = ['1','2','3']
+		// 初始化 成员行为
+		$scope.doen =($event) => {
+			//TODO: 按回车键添加到任务列表中 
+		}
+		$scope.dele =() => {
+			// TODO: 点击删除当前列表行
+		}
+		$scope.eidi = () => {
+			// TODO: 双击弹出input 文本框
+		}
 	}])
 
 })(angular);
