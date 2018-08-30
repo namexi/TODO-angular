7d0af6f4f29ad6a31ee70f2d78e1ea6b1bde9723
namexi <406633257@qq.com>
2018-08-30 14:31:01 +0800

    08-持久化

--- js/app.js
+++ js/app.js
@@ -15,17 +15,32 @@
 		// { id : 2, text : 2, state : false},
 		// { id : 3, text : 3, state : false}
 		]
+		let idx = 0
 		// 初始化 成员行为
 		$scope.doen = ($event) => {
-			let idx = 3
+			
 			//TODO: 按回车键添加到任务列表中 
 			if($event.keyCode === 13  && $scope.input) {
-				let todo = {id : idx++, text : $scope.input, state : false}
-				$scope.todoList.push(todo)
+				idx++
+				let todos = {id:idx, text : $scope.input, state : false}
+				//持久化
+				$scope.todoList.push(todos)
+				// 将数据添加到本地储存
+				localStorage.setItem('pagecount',JSON.stringify($scope.todoList))
+				//==> [{}]
+				
 				$scope.input = ''
 			}
 		}
-		$scope.dele = () => {
+		// 读取本地储存数据
+			if (localStorage.pagecount) {
+				let todo = localStorage.getItem('pagecount')
+				todo = JSON.parse(todo)
+				todo.forEach(element => {
+					$scope.todoList.push(element)
+				})
+		}
+	 	$scope.dele = () => {
 		
 			// TODO: 点击删除当前列表行
 			
