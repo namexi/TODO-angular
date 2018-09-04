3aabc885b46aa5bfd562c50e923782f2d7b937a3
namexi <406633257@qq.com>
2018-09-04 10:01:52 +0800

    gh

--- js/api.js
+++ js/api.js
@@ -34,3 +34,23 @@
 		data.push(todos)
 		return data
 	}
+
+	// 全选与不全选 （计数法）
+	function checkedAllOrNone(anScope,that) {
+		// 获取选择数
+		anScope.index = localStorage.checkAll? localStorage.checkAll : 0
+		// 获取当前元素的索引
+		let idx = anScope.todoList.indexOf(that)
+		// 设置当前元素选择状态为当前元素绑定的状态
+		anScope.todoList[idx].state = that.state
+		// 将数据持久化
+		setStorage('pagecount',anScope.todoList)
+		// 让选择数根据选择状态变化
+		if (that.state) {
+			anScope.index++
+		}else if(localStorage.checkAll > 0) {
+				anScope.index--
+		}
+		// 将选择数返回给外界
+		return anScope.index
+	}
